d2af512c80583363fb24b609414d96ff82d61e24
Oliver Hertzberg <oliverhertzberg@hotmail.com>
2023-02-09 15:25:27 +0200

    useState_setCounter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-const Hello = ({name, age}) => {
-  const bornYear = () => new Date().getFullYear() - age
-  
-  return (
-    <div>
-    <p>
-    Hello {name}, you {age} years old
-    </p>
-    <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
+import { useState } from "react"
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26+10} />
-      <Hello name={name} age={age} />
-    </div>
+  const [counter, setCounter] = useState(0)
+
+  setTimeout(
+    () => setCounter(counter+1),
+    1000
+  )
+//console.log('rendering', counter)
+  return(
+    <div>{counter}</div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
+let counter = 1
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+
+
