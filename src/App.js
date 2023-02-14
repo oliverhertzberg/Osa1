27fdaca39c429947e51d080049b517b70708c029
Oliver Hertzberg <oliverhertzberg@hotmail.com>
2023-02-14 15:03:02 +0200

    tehtävä1.12

--- src/App.js
+++ src/App.js
@@ -1,106 +1,36 @@
-import { useState } from "react"
-import styles from "./styles.css"
+import react from 'react'
+import reactDOM from 'react-dom'
+import styles from './styles.css'
 
-
-const Button = ({goodClick, neutralClick, badClick}) => {
-  return (
-    <div className="buttons">
-      <button  onClick={goodClick}>good</button>
-      <button  onClick={neutralClick}>neutral</button>
-      <button  onClick={badClick}>bad</button>
-    </div>
-  )
-} 
-
-const StatisticLine = (props) => {
-      return (
-      <table>
-        <tbody>
-          <tr>
-            <th>good</th>
-            <td>{props.good}</td>
-          </tr>
-          <tr>
-            <th>neutral</th>
-            <td>{props.neutral}</td>          
-          </tr>
-          <tr>
-            <th>bad</th>
-            <td>{props.bad}</td>
-          </tr>
-          <tr>
-            <th>all</th>
-            <td>{props.all}</td>          
-          </tr>
-          <tr>
-            <th>average</th>
-            <td>{props.Average}</td>          
-          </tr>
-          <tr>
-            <th>positive</th>
-            <td>{props.Positive} %</td>          
-          </tr>
-        </tbody>
-      </table>
-      )
-}
-
-
-
-const Statistics = ({ good, neutral, bad, all, Average, Positive }) => {
-  const feedback = (good+neutral+bad)
-  if(feedback) {
-    return (
-      <div>
-        <StatisticLine good={good} bad={bad} neutral={neutral} all={all} Average={Average} Positive={Positive}/>
-      </div>
-    )
-  }
-  return (
-    <div>
-      <p className="statistics">No feedback given</p>
-    </div>
-  )
-
-}
-
+import { useState } from 'react'
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand',
+    'Premature optimization is the root of all evil',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is the same as if a doctor would refure to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+    const [selected, setSelected] = useState(0)
 
-  const handleGoodClick = () => {
-    console.log("good:",good + 1)
-    setGood(good + 1)
-  }
+    
 
-  const handleNeutralClick = () => {
-    console.log("neutral:",neutral + 1)
-    setNeutral(neutral + 1)
-  }
+    const AnecdoteHandler = () => {
+      console.log("value of selected: ", selected + 1)
+      setSelected((Math.floor(Math.random() *(anecdotes.length))))
+    }////(random integervalue between 0 and 1)*items in the array
 
-  const handleBadClick = () => {
-    console.log("bad:",bad + 1)
-    setBad(bad + 1)
-  }
-
-  const Average = (good - bad)/(good+bad+neutral);
-  //calculating the average
-
-  const Positive = (good/(good+bad+neutral))*100
-  //calculating percentage of positive reviews
-  
-
-  return (
-    <div>
-      <h1>give feedback</h1>
-      <Button goodClick={handleGoodClick} neutralClick={handleNeutralClick} badClick={handleBadClick}/>
-      <h1>statistics</h1>
-      <Statistics good={good} bad={bad} neutral={neutral} all={good+bad+neutral} Average={Average} Positive={Positive}/>
-    </div>
-  )
+    return (
+      <div>
+        {anecdotes[selected]}
+        <br></br>
+        <button onClick={AnecdoteHandler}>next anecdote</button>
+      </div>
+    )
 }
-
 
 export default App
